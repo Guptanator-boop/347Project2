c9076c89025472c74dfc72e19a8e46a7f66ea00b
Guptanator-boop <KushalGupta@gmail.com>
2020-12-10 14:18:03 -0500

    more data

--- make.sql
+++ make.sql
@@ -1,1 +1,6 @@
-CREATE DATABASE today CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+DROP DATABASE IF EXISTS reviews;
+DROP USER IF EXISTS reviews_user@localhost;
+
+CREATE DATABASE reviews CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE USER reviews_user@localhost IDENTIFIED BY 'kush';
+GRANT ALL PRIVILEGES ON reviews.* TO reviews_user@localhost;
--- schema.sql
+++ schema.sql
@@ -2,6 +2,7 @@ DROP TABLE IF EXISTS memory;
 
 CREATE TABLE review (
     username TEXT,
+    movie_name TEXT,
     rating INT,
     message TEXT, 
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
